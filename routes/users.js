01e08e2bc7ea6cecec4d42ec8f94a75b81887e37
Yauheni5 <yauheni5@github.com>
2023-01-29 14:33:20 +0300

    fix: edit profile

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,8 @@ usersRoutes.get('/me', getUserInfo);
 
 usersRoutes.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    email: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    email: Joi.string(),
   }),
 }), updateProfile);
 
